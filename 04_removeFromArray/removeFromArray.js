6bd8765c7385772210cddfea725c11eebbab625e
VinzCanlas <canlas.vinz@gmail.com>
2023-05-21 23:43:22 +0800

    finished the javascript fundamentals 4 project

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+let removeFromArray = function(array, ...args) {
+    return array.filter(item => !args.includes(item))
 };
 
+removeFromArray([1, 2, 3, 4], 3)
 // Do not edit below this line
 module.exports = removeFromArray;
